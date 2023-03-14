77a0887ee1245541cc05943785b1a834821c20fb
Ian <63926745+Ianfhca@users.noreply.github.com>
2023-03-14 17:35:35 +0100

    P5_v2

---
+++ P5/P5_ADC_v2/timers.h
@@ -0,0 +1,11 @@
+// Exporta funciones invocadas en otroos modulos
+
+// Variables
+
+// funciones
+void inic_Timer7 ();
+void cronometro();
+void inic_crono();
+void Delay_ms(int milisegundos);
+void Delay_us(int microsegundos);
+void inic_Timer5();
