253e015349ddde2a7679c31bd6cfc67460bfe488
LuisEduardoCastillo <117465431+LuisEduardoCastillo@users.noreply.github.com>
2023-04-25 17:49:59 +0200

    Update memoria.c

--- P7/memoria.c
+++ P7/memoria.c
@@ -1,11 +1,13 @@
 #include "p24HJ256GP610A.h"
 
-const unsigned char Mens_LCD_1[]={"- DSE 2021-22 --"};  //linea 1.1, 16 caracteres
+const unsigned char Mens_LCD_1[]={"- DSE 2021-22 --"""};  //linea 1.1, 16 caracteres
 const unsigned char Mens_LCD_2[]={"--- Pulsa S3  --"};  //linea 2.1, 16 caracteres
 const unsigned char Mens_LCD_3[]={"---- LCD_v1 ----"};  //linea 1.2, 16 caracteres
 const unsigned char Mens_LCD_4[]={"  S4 comienza --"};  //linea 2.2, 16 caracteres
 const unsigned char Mens_LCD_5[]={"P =    , T =    "};  //linea 1.3, 16 caracteres
 const unsigned char Mens_LCD_6[]={"Crono: --:--,-  "};  //linea 2.3, 16 caracteres
+
+
 
 unsigned char Ventana_LCD[2][16];
 
